407a81b98d4511977bd8e3674086ef5fddff0b38
Mattias Zurkovic <mattias@zurkovic.com>
2016-02-06 14:56:27 -0800

    Add Firebase

--- config/environment.js
+++ config/environment.js
@@ -4,6 +4,8 @@ module.exports = function(environment) {
   var ENV = {
     modulePrefix: 'channel-chat',
     environment: environment,
+    contentSecurityPolicy: { 'connect-src': "'self' https://auth.firebase.com wss://*.firebaseio.com" },
+    firebase: 'https://channel-chat.firebaseio.com/',
     baseURL: '/',
     locationType: 'auto',
     EmberENV: {
